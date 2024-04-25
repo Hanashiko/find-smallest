4538de5c5c3514f6747428b2c20fdaefc89e435e
Hanashiko <129686115+Hanashiko@users.noreply.github.com>
2024-04-25 21:10:51 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+def find_smallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+print(find_smallest([9,3,5,22,4,12,98]))
